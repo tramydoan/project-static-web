65774ea622131a5c4ea347fea75d9e2f3b3dc185
tramydoan <pbe.dontcry.dd@gmail.com>
2019-10-24 14:57:04 +0700

    init

--- js/script.js
+++ js/script.js
@@ -1,8 +1,88 @@
 $(function() {
-
 	//Gọi header.html, footer.html, menu.html
 	$('#header').load('header.html');
-	$('#menu').load('menu.html');
 	$('#footer').load('footer.html');
-
 });
+
+$(function() {
+	$.getJSON('js/db.json', function(data) {
+		data.sale.forEach( function(item, index) {
+			$('#sale').append(`
+				<div id="sale-section">
+				<div class="sale-img">
+				<img src="${item.image}">
+				<p class="sale-tag">UP TO ${item.sale} OFF</p>
+				</div>
+				<div class="sale-info">
+				<p>${item.name}</p>
+				<p>${item.price}</p>
+				</div>
+				</div>
+				`);
+		});
+		data.new.forEach( function(item, index) {
+			$('#new').append(`
+				<div id="new-section">
+				<div class="new-img">
+				<img src="${item.image}">
+				</div>
+				<div class="info">
+				<p>${item.name}</p>
+				<p>${item.price}</p>
+				</div>
+				</div>
+				`);
+		});
+
+
+		data.popular.forEach( function(item, index) {
+			$('#popular').append(`
+				<div id="popular-section">
+				<div class="popular-img">
+				<img src="${item.image}">
+				</div>
+				<div class="info">
+				<p>${item.name}</p>
+				<p>${item.price}</p>
+				</div>
+				</div>
+				`);
+		});
+
+		data.newarivals.forEach( function(item, index) {
+			$('#newarivals-item').append(`
+				<div id="newarivals-section">
+				<div class="newarivals-img">
+				<img src="${item.image}">
+				</div>
+				<div class="info">
+				<p>${item.name}</p>
+				<p>${item.price}</p>
+				</div>
+				</div>
+				`);
+		});
+
+
+	})
+});
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+}
